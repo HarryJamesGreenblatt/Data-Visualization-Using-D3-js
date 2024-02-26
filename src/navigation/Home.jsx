375a648a6ac4b43e151a9f0597d0fa08e53363c5
HarryJamesGreenblatt <HarryJamesGreenblatt@ServiceCentricSolutions.com>
2024-02-25 18:04:18 -0800

    modified:   src/navigation/Home.jsx modified:   src/navigation/SVG.jsx

--- src/navigation/Home.jsx
+++ src/navigation/Home.jsx
@@ -1,31 +1,42 @@
+import {nanoid} from "nanoid"; 
+import { useLocation, Link } from "react-router-dom";
+
+
 export default function Home( { videos, withRespectTo } ) {
 
+    // Filtering videos array to find a video whose title matches withRespectTo
     const courseVideo = videos.filter( 
         video => video.title === withRespectTo
     )[0]
 
+    // Rendering the Home component
     return(
+
+        // Checking if withRespectTo and courseVideo exist
         withRespectTo && courseVideo
         ?
+        // If they exist, render the video details
         <div className="home-container">
             <h3>{courseVideo.title}</h3>
             <div className="home-content-container">
                 <div className="home-content-iframe-container">
                     <iframe 
-                        src={courseVideo.url}
-                        title={courseVideo.title}
+                        src={courseVideo.url} // Video URL
+                        title={courseVideo.title} // Video title
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                        allowFullScreen
-                        id={withRespectTo === "React + D3.js" ? "wattenberg-frame": ""}
+                        allowFullScreen // Allow fullscreen
+                        id={withRespectTo === "React + D3.js" ? "wattenberg-frame": ""} // Specific id for "React + D3.js"
                     >
                     </iframe>
                 </div>
                 <div className="home-content-iframe-description-container">
-                    {courseVideo.description}
+                    {/* Video description */}
+                    {courseVideo.description} 
                 </div>
             </div>
         </div>
         :
+        // If they don't exist, render a message to select a course
         <div className="home-container">
             <div style={{marginTop: "2em", justifyContent:"center"}} className="home-content-container">
                 <p>Select from one of the courses listed below:</p>
--- src/navigation/SVG.jsx
+++ src/navigation/SVG.jsx
@@ -1,36 +1,39 @@
 import {useLocation} from "react-router-dom";
 import {nanoid} from "nanoid"; 
 
+
 export default function SVG( { notes, withRespectTo, clearOnClick } ) {
 
+    // Getting the current pathname
     const { pathname } = useLocation();
 
-
-
+    // Filtering notes array to find a note whose pathname and withRespectTo match the current ones
     const svgNotes = notes && withRespectTo && notes.filter( 
         note => note.pathname === pathname && note.withRespectTo === withRespectTo 
     )[0];
 
-
+    // Mapping keyTakeAways array to create a list of takeaways
     const takeAways = svgNotes && withRespectTo && svgNotes.keyTakeAways.map(takeAway => 
-        <li key={nanoid()}> {takeAway} </li>
+        <li key={nanoid()}> {takeAway} </li> // Unique key for each takeaway
     )
 
-
+    // Rendering the SVG component
     return(
-
         <div className="notes-container">
             <div className="notes-header-container">
                 <h3>
                 {
+                    // If svgNotes and withRespectTo exist, show the note title
                     svgNotes && withRespectTo
                     ?
                     `SVG notes with respect to --  ${withRespectTo}`
                     :
+                    // Otherwise, show a default title
                     "Scalable Vector Graphics (SVG)"
                 }
                 </h3>
                 {
+                    // If svgNotes and withRespectTo exist, show a button to clear notes
                     svgNotes 
                     && 
                     withRespectTo 
@@ -40,35 +43,43 @@ export default function SVG( { notes, withRespectTo, clearOnClick } ) {
             </div>
         
             {
+                // If svgNotes and withRespectTo exist, show the note details
                 svgNotes && withRespectTo
                 ?
                 <div className="notes-content-container">
                     <div className="notes-content-left-column-container">
                         {
+                            // If img exists, show the image
                             svgNotes.img
                             ?
                             <img 
                                 src={svgNotes.img}
                                 style={
+                                    // Specific style for "html"
                                     withRespectTo === "html" 
                                         ? {width:"320px"}
                                         : null
                                 }    
                             />
                             :
+                            // Otherwise, show the component
                             svgNotes.component
                         }
-                        <pre>{svgNotes.description}</pre>
+                        {/* Note description */}
+                        <pre>{svgNotes.description}</pre> 
                     </div>
                     <div className="notes-content-right-column-container">
                         <ul>
-                            {takeAways}
+                            {/* List of takeaways */}
+                            {takeAways} 
                         </ul>
                     </div>
                 </div>
                 :
+                // If svgNotes or withRespectTo do not exist, show a message to select a note topic
                 <div className="selection-container">
-                    <iframe src="https://en.wikipedia.org/wiki/SVG"/>
+                    {/* Wikipedia page for SVG */}
+                    <iframe src="https://en.wikipedia.org/wiki/SVG"/> 
                     <p>Select one of the <span>note topics</span> below for a deeper dive:</p>
                 </div>
             }
